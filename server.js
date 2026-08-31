5a8b1193c78363c5bd0c30756edc18c05c0fd817
rifqixrpl3 <rifkifalih30@gmail.com>
2026-08-31 12:02:55 +0700

    Fix port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const { URL } = require("url");
 
-const port = 4173;
+const port = 3000;
 const root = __dirname;
 const allowedHost = "everythingjkt48.my.id";
 const mimeTypes = { ".html": "text/html; charset=utf-8", ".js": "text/javascript; charset=utf-8", ".css": "text/css; charset=utf-8" };
